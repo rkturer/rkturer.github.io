4b5760b495ec4497b530596e662bab357e5be1fb
rkturer <rkt26@icloud.com>
2025-09-25 15:18:17 -0400

    Update navbar.js

--- js/navbar.js
+++ js/navbar.js
@@ -2,5 +2,5 @@ fetch('pages/navbar.html')
       .then (response=> response.text())
       .then(data=>{
         document.getElementById('navbar-placeholder').innerHTML = data;
-          )}
+          })
       .catch(error=>console.error('Error loading nav bar:', error));
